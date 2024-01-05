a39980e35cb36f0d453113e91c58fdd84a1bc2bd
Agata Gradecka <agata.gradecka@gmail.com>
2024-01-05 16:48:00 +0100

    feat: add some footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,13 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="flex flex-col lg:flex-row items-center">
+        <div className="flex flex-col">
+          <p className="text-xs">
+            To nie jest blog kulinarny. Nie ma tu zdjęć ani wyszukanych opisów.
+            Traktuję tę stronę jako notes z moimi ulubionymi przepisami, do
+            których wracam... żeby nie musieć ich szukać za każdym razem ;)
+            Każdy przepis ma odnośnik do źródła... jeśli je pamiętam.
+          </p>
           <p className="text-xs">© shuimanao</p>
         </div>
       </Container>
