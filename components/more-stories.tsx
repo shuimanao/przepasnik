2223f3b7dfdf29f44f02c339c11f1a822710a08f
Agata Gradecka <agata.gradecka@gmail.com>
2025-06-14 23:06:48 +0200

    feat: make whole card clickable

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -18,16 +18,11 @@ const MoreStories = ({ posts: initialPosts }: Props) => {
     <section>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-32">
         {filteredPosts.map(post => (
-          <article
-            key={post.slug}
-            className="border border-neutral-200 rounded-lg p-6 hover:border-neutral-400 transition-colors bg-white shadow-sm hover:shadow-md"
-          >
-            <h3 className="text-xl font-semibold leading-snug">
-              <Link href={`/posts/${post.slug}`} className="hover:underline">
-                {post.title}
-              </Link>
-            </h3>
-          </article>
+          <Link href={`/posts/${post.slug}`} className="block h-full" key={post.slug}>
+            <article className="border border-neutral-200 rounded-lg p-6 hover:border-neutral-400 transition-colors bg-white shadow-sm hover:shadow-md h-full flex items-center">
+              <h3 className="text-xl font-semibold leading-snug">{post.title}</h3>
+            </article>
+          </Link>
         ))}
       </div>
     </section>
